d2415c55ddea091e773324264823c0a160bf6525
theycallmek <theycallmekenny@gmail.com>
2022-07-24 19:15:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,15 @@ def find_promos():
             if matches.get_text() != r'CLAIMED':
                 label_text = matches.get_text().encode("utf-8")
                 print(f'Found {label_text} quest card. Clicking')
+            match_text = matches.get_text().encode('utf-8')
+            print(match_text)
+            if match_text == 'WATCH':
+                matches.click()
+                print(f'WATCH keyword found in quest cards. Clicking')
+            if match_text == ('CLAIM'):
+                print(f'CLAIM keyword found in quest cards. Clicking')
                 matches.click()
                 return   # We only need to click one card so return.
-
 
 # Drive it like you stole it
 while True:
@@ -39,22 +45,32 @@ while True:
         # Open daily quest cards 1st step
         if frozen_poco("com.razer.cortex:id/open_button").exists():
             frozen_poco("com.razer.cortex:id/open_button").click()
+            print('com.razer.cortex:id/open_button')
             
         # Open daily quest cards 2nd step
         if frozen_poco("com.razer.cortex:id/btn_open_now").exists():
             frozen_poco("com.razer.cortex:id/btn_open_now").click()
+            print('com.razer.cortex:id/btn_open_now')
             
         # Daily quest cards
         if frozen_poco("com.razer.cortex:id/btn_dqc_view_go").exists():
             frozen_poco("com.razer.cortex:id/btn_dqc_view_go").click()
+            print('razer.cortex:id/btn_dqc_view_go')
+        
+        # Level up pop up
+        if frozen_poco("com.razer.cortex:id/btn_black_positive_button").exists():
+            frozen_poco("com.razer.cortex:id/btn_black_positive_button").click()
+            print('com.razer.cortex:id/btn_black_positive_button')
         
         # Close video ad method 1
         if frozen_poco("com.razer.cortex:id/ia_tv_close_button").exists():
             frozen_poco("com.razer.cortex:id/ia_tv_close_button").click()
+            print('com.razer.cortex:id/ia_tv_close_button')
             
         # Close video ad method 2
         if frozen_poco("close-icon").exists():
             frozen_poco("close-icon").click()
+            print('close-icon')
         
         # Get ad time remaining
         if frozen_poco("ia_tv_remaining_time").exists():
@@ -62,27 +78,43 @@ while True:
             print(f'Ad playing. Time remaining: {time_left}')
                     
         # Start watching ad    
-        if frozen_poco("com.razer.cortex:id/btn_watch_ad").exists():
-            frozen_poco("com.razer.cortex:id/btn_watch_ad").click()
+        if frozen_poco("com.razer.cortex:id/btn_watch").exists():
+            frozen_poco("com.razer.cortex:id/btn_watch").click()
+            print('com.razer.cortex:id/btn_watch_ad')
         
         # Continue button
         if frozen_poco("com.razer.cortex:id/btn_positive").exists():
             frozen_poco("com.razer.cortex:id/btn_positive").click()
+            print('com.razer.cortex:id/btn_positive')
 
         # Claim button
         if frozen_poco("com.razer.cortex:id/btn_claim").exists():
             frozen_poco("com.razer.cortex:id/btn_claim").click()
+            print('com.razer.cortex:id/btn_claim')
 
-        # OK button?
-        if frozen_poco("com.razer.cortex:id/btn_green").exists():
-            frozen_poco("com.razer.cortex:id/btn_green").click()   
+        print(frozen_poco("com.razer.cortex:id/btn_green").get_text().encode('utf-8'))
+        # Doesn't work properly. Need to revisit this.'
+        if frozen_poco("com.razer.cortex:id/btn_green").get_text().encode('utf-8') == 'CLAIM':
+        # if frozen_poco("com.razer.cortex:id/btn_green").exists():
+            if frozen_poco("com.razer.cortex:id/btn_green").get_text().encode('utf-8') == 'CLAIM':
+                frozen_poco("com.razer.cortex:id/btn_green").click()
+                print('com.razer.cortex:id/btn_green text = CLAIM. Clicking.')
+        
+        # Another pop up claim button
+        if frozen_poco("com.razer.cortex:id/btn_green_positive_button").exists():
+            frozen_poco("com.razer.cortex:id/btn_green_positive_button").click()
+            print('com.razer.cortex:id/btn_green_positive_button')
             
         # Closes window asking for Google Play Review
         if frozen_poco("com.razer.cortex:id/subtitle").exists():
             if frozen_poco("com.razer.cortex:id/subtitle").get_text() == 'SEND US FEEDBACK ON GOOGLE PLAY STORE':
                 keyevent("BACK")
+                print(f'com.razer.cortex:id/subtitle.get_text() = {frozen_poco("com.razer.cortex:id/subtitle").get_text()}')
 
         if frozen_poco("android.view.View").offspring("android.widget.Button").exists():
             frozen_poco("android.view.View").offspring("android.widget.Button").click()
+            print('android.view.View.offspring .. android.widget.Button')
+
+
 
 
